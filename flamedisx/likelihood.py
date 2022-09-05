5324f8f6680b2e15d7fba6fecc2f9da1f8bd8d6d
Robert James <robert.james.19@ucl.ac.uk>
2022-09-05 19:54:40 +0200

    Option to suppress warnings during fits.

--- flamedisx/likelihood.py
+++ flamedisx/likelihood.py
@@ -579,7 +579,8 @@ class LogLikelihood:
                 return_errors=False,
                 nan_val=float('inf'),
                 optimizer_kwargs=None,
-                allow_failure=False):
+                allow_failure=False,
+                suppress_warnings=False):
         """Return best-fit parameter dict
 
         :param guess: Guess parameters: dict {param: guess} of guesses to use.
@@ -640,6 +641,7 @@ class LogLikelihood:
             return_errors=return_errors,
             optimizer_kwargs=optimizer_kwargs,
             allow_failure=allow_failure,
+            suppress_warnings=suppress_warnings,
         ).minimize()
         if get_lowlevel_result or get_history:
             return res
@@ -685,6 +687,7 @@ class LogLikelihood:
             optimizer_kwargs=None,
             use_hessian=True,
             allow_failure=False,
+            suppress_warnings=False
     ):
         """Return frequentist limit or confidence interval.
 
@@ -784,6 +787,7 @@ class LogLikelihood:
                 use_hessian=use_hessian,
                 optimizer_kwargs=optimizer_kwargs,
                 allow_failure=allow_failure,
+                suppress_warnings=suppress_warnings,
 
                 # To IntervalObjective
                 target_parameter=parameter,
